797f2221f589acebbadddba84221aa311388e7f2
mian <mianhamzadeveloper@gmail.com>
2023-04-05 15:55:52 -0700

    message

---
+++ script.js
@@ -0,0 +1,152 @@
+//01 project  Maximum between two numbers.
+// var num1 =+ prompt("Enter number 1");
+// var num2 =+ prompt("Enter number 2");
+
+
+
+// if (num1 > num2) {
+// console.log("This Number is Maximum", num1 ); }
+// else
+// {console.log("This Number is Maximum", num2 );
+// }
+
+// 02 project    Maximum between three numbers.
+// var num3 =+ prompt("Enter Number 1")
+// var num4 =+ prompt("Enter Number 2") 
+// var num5 =+ prompt("Enter Number 3")
+
+// if (num3 > num4)
+// {  console.log("This Number is Maximum", num3)   }
+// else if (num4 > num5)
+// { console.log("This Number is Maximum" , num4) }
+// else 
+// { console.log("This Number is Maximum" , num5)  }
+
+//03 project  check weather a number is negative , psitive or zero.
+// var num6 =+ prompt("Enter Number")
+
+// if (num6 < 0 )
+// { console.log("This Number is Negative" , num6) }
+// else if (num6 > 0)
+// { console.log("This Number is Positive" , num6) }
+// else 
+// { console.log("This Number is Equal to Zero" , num6) }
+
+//04 project   check weather a number is divisible by 5 & 11 or not.
+// var b =+ prompt("Enter Any Number");
+// if (b % 5 == 0 &&  b % 11 == 0 )
+// { console.log("This Number is Dividable by 5 & 11" , b) }
+// else {console.log("This Number is not Dividable by 5 & 11" , b)       }
+
+
+//05 Project   check weather a number is even or odd.
+// var num7 =+ prompt("Enter a Number")
+// if  ( num7 % 2 == 0)
+// { console.log("This Number is Even." , num7 )   }
+// else {console.log("This Number is Odd." , num7 ) }
+
+
+//06 Project   check weather a year is leap or not.
+// var num8 =+ prompt("Enter any Year")
+// if ( num8 % 4 == 0 )
+// { console.log("This Year is Leap" , num8)  }
+// else { console.log("This Year is not Leap" , num8) }
+
+
+//07 Project   check weather a character is alphabet or not.
+// var c = prompt("Enter Something")
+// if (( c >= "a" && c <= "z" )||( c >= "A" && c <= "Z" ))
+// { console.log("This Character Is Alphabet" , c) }
+// else 
+// { console.log("This Character Is Not Alphabet" , c)  };
+
+//08 Project   Input any alphabat and check weather it is vowel or constant.
+// var num9 = prompt("Enter any Alphabat")
+// if ( num9 == "a" || num9 == "e" || num9 == "i" || num9 == "o" || num9 == "u" ||
+//   num9 == "A" || num9 == "I" || num9 == "O" || num9 == "E" || num9 == "U"  )
+//   { console.log("This Character is Vowel", num9 ) }
+//   else {console.log("This Character is not Vowel", num9 )  }
+
+//09 Project    Input any character and check it is alphabet, digit or special character.
+// var num10 = prompt("Enter any Character")
+// if (num10 >= 0)
+// { console.log("This is Digits" , num10) }
+// else if ((num10 >= "a" && num10 <= "z") || (num10 >= "A" && num10 <= "Z"))
+// { console.log("This is Alphabat" , num10) }
+// else { console.log("This is Special Character" , num10) }
+
+//10 Project   check the input character is upprecase or lowercase alphabat.
+// var num11= prompt("Enter Character")
+// if (num11 >= "a" && num11 <= "z" )
+// {console.log("This Alphabat is Lowercase" , num11)  }
+// else if (num11 >= "A" && num11 <= "Z")
+// {console.log("This Alphabat is Uppercase" , num11)  }
+// else 
+// {console.log("Irrelevent" , num11)  }
+
+//11 Project  Input week number and print week day.
+// var day =+ prompt("Enter Days Number")
+// if (day==1)
+// console.log("MONDAY" , day)
+
+// else if (day==2)
+// console.log("TUESDAY" , day)
+
+// else if (day==3)
+// console.log("WEDNESDAY" , day)
+
+// else if (day==4)
+// console.log("THURSDAY" , day)
+
+// else if (day==5)
+// console.log("FRIDAY" , day)
+
+// else if (day==6)
+// console.log("SATURDAY" , day)
+
+// else if(day==7)
+// console.log("SUNDAY" , day)
+
+// else
+// console.log("IRRELEVENT" , day)
+
+//12 Project   Input month number and print number of days in that month.
+// var month =+ prompt("Enter Numbers of Months")
+// if (month == 1)
+// { console.log("31  JANUARY" , month)}
+
+// else if (month == 2)
+// { console.log("28   FEBURARY" , month)}
+
+// else if (month == 3)
+// { console.log("31 MARCH " , month)}
+
+// else if (month == 4)
+// { console.log("30  APRIL" , month)}
+
+// else if (month == 5)
+// { console.log("31   MAY" , month)}
+
+// else if (month == 6)
+// { console.log("30   JUNE" , month)}
+
+// else if (month == 7)
+// { console.log("31   JULY" , month)}
+
+// else if (month == 8)
+// { console.log("31   AUGUST" , month)}
+
+// else if (month == 9)
+// { console.log("30  SEPTEMBER" , month)}
+
+// else if (month == 10)
+// { console.log("31   OCTOBER" , month)}
+
+// else if (month == 11)
+// { console.log("30   NOVEMBER" , month)}
+
+// else if (month == 12)
+// { console.log("31  DECEMBER" , month)}
+
+// else
+// { console.log("IRRELEVENT" , month)}
